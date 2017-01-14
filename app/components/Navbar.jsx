cfba6557682c918d949786acada16f22f1292914
Brandon Brigance <brandon.brigance@gmail.com>
2017-01-14 00:12:02 -0600

    style navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -13,15 +13,43 @@ let {Link, IndexLink} = require('react-router');  //load the Link object
 //  }
 //});
 
-let Navbar = () => {
-  return(
-    <div>
-      <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>  {/*IndexLink ensure active styles are only applied when the linked component is active. * see notes below*/}
-      <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>             {/*the 'to' property represents the path the link leads to*/}
-      <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>       {/*activeClassName gives the class to the active component.  This allows you to manipulate only the class that is active with activeStyle.*/}
-    </div>
-  );
-};
+let Navbar = React.createClass({
+  onSearch: function(e){
+    e.preventDefault();
+    alert('no functionality yet');
+  },
+  render: function(){
+    return(
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}} class="menu-text">Get Weather</IndexLink>
+            </li>
+            <li>
+              <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+            </li>
+            <li>
+              <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>   {/*with a form, we must use React.createClass instead of arrow functions*/}
+            <ul className="menu">
+              <li>
+                <input type="search" placeholder="Search Weather"/>
+              </li>
+              <li>
+                <input type="submit" className="button" value="Get Weather"/>
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    );
+  }
+})
 
 module.exports = Navbar;
 
