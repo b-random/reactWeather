54af804e3d59128b01f1306b2d25cef3a8747125
Brandon Brigance <brandon.brigance@gmail.com>
2017-01-13 18:32:34 -0600

    rename example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -8,7 +8,7 @@ let React = require('react');
 //  }
 //});
 
-let Examples = () => {return <h2>Example component</h2>};
+let Examples = () => {return <h2>Examples</h2>};
 
 
 module.exports = Examples;
