ba3453d417099764f34a910b808255ce7acd4717
Brandon Brigance <brandon.brigance@gmail.com>
2017-01-15 19:38:59 -0600

    Add error modal w/ styling

---
+++ app/components/ErrorModal.jsx
@@ -0,0 +1,19 @@
+let React = require('react');
+
+let ErrorModal = React.createClass({
+  render: function(){
+    return(
+      <div id="error-modal" className="reveal tiny text-center" data-reveal> //all classes/id's are required for error modal
+        <h4>Some Title</h4>
+        <p>The error message!</p>
+        <p>
+         <button className="button" data-close>
+           Okay
+         </button> //data-close class closes the modal on click
+        </p>
+      </div>
+    );
+  };
+});
+
+module.exports = ErrorModal;
